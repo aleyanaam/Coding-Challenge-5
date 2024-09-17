5788911282dd9f7b6355d5caf11eadf50b9660dc
Aleyana McLeod <aleyanam@usf.edu>
2024-09-17 18:22:48 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,13 @@
+// Task 1
+// Initialization of array with product objects 
+const inventory = [
+
+{ name: 'Espresso', price: 3, quantity: 10 },
+    
+{ name: 'Latte', price: 4, quantity: 5 },
+    
+{ name: 'Cappuccino', price: 4, quantity: 6 },
+    
+{ name: 'Mocha', price: 5, quantity: 4 }
+    
+];
